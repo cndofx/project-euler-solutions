e8bea8605b45b6ecc4b4c38f4efdcd88530acf5a
Trevor Tilby <trevor.tilby@gmail.com>
2021-12-14 09:28:01 -0700

    python problem 3

---
+++ 03 - Largest Prime Factor/python/main.py
@@ -0,0 +1,16 @@
+import math
+
+def largestPrimeFactor(num):
+    factor = 3
+    previous = 1
+    for i in range (1, int(math.sqrt(num))+1):
+        if num % factor == 0:
+            previous = factor
+            while num % factor == 0:
+                num /= factor
+        factor += 2
+
+    return previous
+
+result = largestPrimeFactor(600851475143)
+print("result: " + str(result))
