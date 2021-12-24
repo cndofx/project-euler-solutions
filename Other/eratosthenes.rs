b25375f84ef3ccff76580a0205fc981482814ce4
Trevor Tilby <trevor.tilby@gmail.com>
2021-12-23 23:25:04 -0700

    sieve of eratosthenes in rust

---
+++ Other/eratosthenes.rs
@@ -0,0 +1,31 @@
+fn main() {
+    sieve(10000);
+}
+
+fn sieve(num: u64) {
+    let mut primes: Vec<bool> = vec![true; (num+1) as usize];
+
+    let mut i: usize = 2;
+    while i.pow(2) as u64 <= num {
+        if primes[i as usize] == true {
+            println!("{} is prime", i);
+            let mut j: usize = i.pow(2);
+            while j as u64 <= num {
+                primes[j] = false;
+                println!("crossed out {}", j);
+                j += i;
+            }
+        }
+        i += 1;
+    }
+
+    let mut i: usize = 2;
+    while i as u64 <= num {
+        if primes[i] == true {
+            println!("{}", i);
+        }
+        i += 1;
+    }
+}
+
+// https://www.geeksforgeeks.org/sieve-of-eratosthenes/
