ce5ecda06a9c6f93cc074ea2a8c4b784984c0311
Trevor Tilby <trevor.tilby@gmail.com>
2021-12-23 19:24:53 -0700

    python problem 6

---
+++ 06 - Sum Square Difference/python/main.py
@@ -0,0 +1,17 @@
+def sumOfSquares(upTo):
+    sum = 0
+    for i in range(1, upTo + 1):
+        sum += pow(i, 2)
+    return sum
+
+def squareOfSum(upTo):
+    sum = 0
+    for i in range(1, upTo + 1):
+        sum += i
+    return pow(sum, 2)
+
+def sumSquareDifference(upTo):
+    return squareOfSum(upTo) - sumOfSquares(upTo)
+
+result = sumSquareDifference(100)
+print(result)
